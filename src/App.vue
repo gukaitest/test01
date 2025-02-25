8e8fcd0c82b147ba2d3850d105c3dbcdaab48501
gukai <1036350534@qq.com>
2025-02-25 18:55:13 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home111s</RouterLink>
+        <RouterLink to="/">Home111222s</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
